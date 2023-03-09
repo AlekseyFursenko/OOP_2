401f4f45f89d6176c6996d54daffeb0ebc9618c5
Phursenko <phursenko@hotmail.com>
2023-03-09 14:00:14 +0300

    Units and heroes sets

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import Units.Intellect.Monk;
+import Units.Intellect.Warlock;
+import Units.Servant.Pesant;
+import Units.Shooter.Archer;
+import Units.Shooter.Shooter;
+import Units.Shooter.XBowman;
+import Units.Unit;
+import Units.Warrior.Robber;
+import Units.Warrior.Spearman;
+import Units.Warrior.Warrior;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Monk hero1 = new Monk();
+        Warlock hero2 = new Warlock();
+        Pesant hero3 = new Pesant();
+        Unit hero4 = new Archer();
+        Unit hero5 = new XBowman();
+        Unit hero6 = new Robber();
+        Warrior hero7 = new Spearman();
+
+    }
+}
